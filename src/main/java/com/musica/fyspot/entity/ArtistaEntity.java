83f2ae3dec62653006317deead139dcdbeff755c
Tiago <tiago.winvli37@aluno.ifce.edu.br>
2026-04-17 20:09:34 -0300

    minha primeira entidade

---
+++ src/main/java/com/musica/fyspot/entity/ArtistaEntity.java
@@ -0,0 +1,17 @@
+package com.musica.fyspot.entity;
+import jakarta.persistence.*;
+import lombok.Data;
+import java.util.Date;
+
+@Entity
+@Table(name="Artista")
+@Data
+public class ArtistaEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nome;
+    private String nacionalidade;
+    private Date dataNascimento;
+
+}
---
+++ src/main/java/com/musica/fyspot/repository/ArtistaRepository.java
@@ -0,0 +1,9 @@
+package com.musica.fyspot.repository;
+
+import com.musica.fyspot.entity.ArtistaEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ArtistaRepository extends
+        JpaRepository< ArtistaEntity,Integer> {
+
+}
